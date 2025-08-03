bbe16a8b1d7147060fcd91a8c0e57b504be88802
VitroidFPV <76877124+VitroidFPV@users.noreply.github.com>
2025-08-03 16:33:46 +0200

    Update: Use Tailwind Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
+import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [
-		sveltekit()
+		tailwindcss(),
+		sveltekit(),
 	],
 	server: {
 		port: 3000 // set the port to 3000
