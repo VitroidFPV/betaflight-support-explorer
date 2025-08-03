d162c15cd5aa1d8fba058605468e55947b7d8ffd
VitroidFPV <76877124+VitroidFPV@users.noreply.github.com>
2025-08-03 16:29:57 +0200

    Update: Clean up tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,6 @@ import { join } from "path";
 import type { Config } from "tailwindcss";
 import forms from "@tailwindcss/forms";
 import typography from "@tailwindcss/typography";
-import { skeleton } from "@skeletonlabs/tw-plugin";
-import { yellowTheme } from "./yellowTheme";
 
 export default {
 	darkMode: "class",
@@ -22,16 +20,5 @@ export default {
 	plugins: [
 		forms,
 		typography,
-		skeleton({
-			themes: {
-				custom: [yellowTheme]
-				// preset: [
-				// 	{
-				// 		name: 'skeleton',
-				// 		enhancements: true,
-				// 	},
-				// ],
-			}
-		})
 	]
 } satisfies Config;
