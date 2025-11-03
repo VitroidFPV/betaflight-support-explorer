ce1f31490446a26bb0e736942100bb48753eb7b0
VitroidFPV <76877124+VitroidFPV@users.noreply.github.com>
2025-11-03 21:04:07 +0100

    Fix: Bring back new tab GitHub links

--- src/components/TargetListItem.svelte
+++ src/components/TargetListItem.svelte
@@ -15,6 +15,8 @@
 	<div class="flex">
 		<a
 			href={`https://github.com/betaflight/config/blob/master/configs/${target.target}/config.h`}
+			target="_blank"
+			rel="noopener noreferrer"
 			class="hover:text-primary-500 w-fit h-fit bg-transparent hover:bg-primary-500/10 aspect-square p-1 rounded-lg"
 		>
 			<Icon src={Github} size="1.5rem" />
